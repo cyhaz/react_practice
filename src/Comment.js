9f91f8e502302458aab55729f87782b0784b3afa
cyhaz <cyhaz@naver.com>
2020-10-18 15:50:06 +0900

    signup.js 생성

---
+++ src/Comment.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Comment extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{`My first React App`}</h1>
+      </div>
+    );
+  }
+}
+
+export default Comment;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("main")
 );
+
+const formatName = (name) => {
+  return name.firstName + " " + name.lastName;
+};
+const name = {
+  firstName: "choi",
+  lastName: "youhyun",
+};
+const element = <h1>Hello, {formatName(name)}!</h1>;
+
+function tick() {
+  const elem = (
+    <div>
+      <h1>Hellos, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+
+  ReactDOM.render(elem, document.getElementById("root"));
+}
+// setInterval(tick, 1000);
+
+// ReactDOM.render(element, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
